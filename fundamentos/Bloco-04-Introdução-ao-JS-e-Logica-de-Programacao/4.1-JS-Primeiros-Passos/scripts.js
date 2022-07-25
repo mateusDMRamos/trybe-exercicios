8e370349f559fc42b300f2722fe1e5647bdd9f3d
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-07-25 11:26:33 -0300

    exercicio para conhecer a estrutura do switch

--- fundamentos/Bloco-04-Introdução-ao-JS-e-Logica-de-Programacao/4.1-JS-Primeiros-Passos/scripts.js
+++ fundamentos/Bloco-04-Introdução-ao-JS-e-Logica-de-Programacao/4.1-JS-Primeiros-Passos/scripts.js
@@ -1,8 +1,15 @@
-let weekDay = "sabado";
+let statusPessoa = "inexistente";
 
-if(weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira"){
-    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
-}
-else {
-    console.log("FINALMENTE, descanso merecido UwU");
+switch(statusPessoa){
+    case "aprovada":
+        console.log("Parabéns, você foi aprovada(o)!");
+        break
+    case "lista":
+        console.log("Você está na nossa lista de espera");
+        break
+    case "reprovada":
+        console.log("Você foi reprovada(o)");
+        break
+    default:
+        console.log("Não se aplica");
 }
