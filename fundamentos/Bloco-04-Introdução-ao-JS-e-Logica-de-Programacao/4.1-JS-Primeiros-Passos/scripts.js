a96776c2c6308ebed3d6537e9a37327c1bd9c959
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-07-25 10:25:02 -0300

    arquivo modificado para trabalhar operações aritméticas

--- fundamentos/Bloco-04-Introdução-ao-JS-e-Logica-de-Programacao/4.1-JS-Primeiros-Passos/scripts.js
+++ fundamentos/Bloco-04-Introdução-ao-JS-e-Logica-de-Programacao/4.1-JS-Primeiros-Passos/scripts.js
@@ -1,13 +1,7 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+let base = 5;
+let height = 8;
+let area = base*height;
+let perimeter = 2*base+2*height;
 
-console.log(typeof patientId);
-console.log(typeof isEnrolled);
-console.log(typeof patientInfo);
-console.log(typeof patientEmail);
-console.log(typeof patientAge);
+console.log(area);
+console.log(perimeter);
