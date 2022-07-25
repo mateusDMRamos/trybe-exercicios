46bf022b59001df9c5da5ce4c901eb30f17dd96f
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-07-25 10:18:33 -0300

    arquivo alterado para estudo de tipagem, caso a variável não tenha sido decalrada, ela terá o tipo 'undefined'

--- fundamentos/Bloco-04-Introdução-ao-JS-e-Logica-de-Programacao/4.1-JS-Primeiros-Passos/scripts.js
+++ fundamentos/Bloco-04-Introdução-ao-JS-e-Logica-de-Programacao/4.1-JS-Primeiros-Passos/scripts.js
@@ -1,13 +1,13 @@
-const myName = "Mateus";
-const birthCity = "Belo Horizonte";
-let birthYear = 1995;
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
 
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
-
-birthYear = 2030;
-console.log(birthYear);
-
-birthCity = "Rio de Janeiro";
-console.log(birthCity);
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
