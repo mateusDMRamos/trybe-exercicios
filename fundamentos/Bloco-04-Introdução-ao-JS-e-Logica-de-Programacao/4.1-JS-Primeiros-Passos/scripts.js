9287c4ece1f53e8af5491a65ae065cee9932ccb0
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-07-25 10:04:42 -0300

    modificação da variável birthYear e da constante birthCity, o erro na alteração de birthCity ocorreu pois ela é constante (não pode ser alterada)

--- fundamentos/Bloco-04-Introdução-ao-JS-e-Logica-de-Programacao/4.1-JS-Primeiros-Passos/scripts.js
+++ fundamentos/Bloco-04-Introdução-ao-JS-e-Logica-de-Programacao/4.1-JS-Primeiros-Passos/scripts.js
@@ -5,3 +5,9 @@ let birthYear = 1995;
 console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = "Rio de Janeiro";
+console.log(birthCity);
