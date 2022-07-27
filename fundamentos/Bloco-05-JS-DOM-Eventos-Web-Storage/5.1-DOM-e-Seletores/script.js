12a626c6aee5b1fd6aa55a6148b2fc23e3d1efdf
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-07-27 09:42:54 -0300

    arquivos alterados para aprender a utilizar o getElementById e alterar eles pelo arquivo JS.

---
+++ fundamentos/Bloco-05-JS-DOM-Eventos-Web-Storage/5.1-DOM-e-Seletores/script.js
@@ -0,0 +1,13 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById("page-title").innerText = "Star Wars";
+
+let paragrafo2 = document.getElementById("second-paragraph");
+paragrafo2.innerText = "Amanheceu, peguei a viola, Botei na sacola, e fui viajar";
+paragrafo2.style.color = "purple";
+paragrafo2.style.background = "yellow";
+
+let subtitulo = document.getElementById("subtitle");
+subtitulo.innerText = "Musica q Gruda:";
+subtitulo.style.fontSize = "30px";
