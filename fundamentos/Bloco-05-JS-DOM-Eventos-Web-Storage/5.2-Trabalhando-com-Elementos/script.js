0072d6d0886711b0981c7fa9ffb1f8d9b0ba9369
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-07-28 11:37:12 -0300

    realizado o segundo exercício

--- fundamentos/Bloco-05-JS-DOM-Eventos-Web-Storage/5.2-Trabalhando-com-Elementos/script.js
+++ fundamentos/Bloco-05-JS-DOM-Eventos-Web-Storage/5.2-Trabalhando-com-Elementos/script.js
@@ -5,6 +5,10 @@ titulo.innerText = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(titulo);
 
 // 🚀 Adicione a tag main com a classe main-content como filho da tag body;
+let main = document.createElement('main');
+main.className = 'main-content';
+document.body.appendChild(main);
+
 // 🚀 Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
 // 🚀 Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
 // 🚀 Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
