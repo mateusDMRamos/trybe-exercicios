31cab164d5f2861e4a58418cd2015f26588e850a
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-07-28 10:17:42 -0300

    realizados exercícios de criação e adição de elementos no documento HTML através do JS

--- fundamentos/Bloco-05-JS-DOM-Eventos-Web-Storage/5.2-Trabalhando-com-Elementos/script.js
+++ fundamentos/Bloco-05-JS-DOM-Eventos-Web-Storage/5.2-Trabalhando-com-Elementos/script.js
@@ -11,3 +11,20 @@ console.log(document.getElementById('elementoOndeVoceEsta').nextSibling);
 console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling);
 
 console.log(document.getElementById('pai').children[2]);
+
+let filha = document.createElement('section');
+filha.className = 'filha';
+
+document.querySelector('#pai').appendChild(filha);
+
+let ondeEstou = document.createElement('section');
+ondeEstou.className = 'onde-estou'
+
+document.querySelector('#elementoOndeVoceEsta').appendChild(ondeEstou);
+
+let filhoDoFilho = document.createElement('section');
+filhoDoFilho.className = 'primeiroFilhoDoFilhoDoFilho';
+
+document.querySelector('#primeiroFilhoDoFilho').appendChild(filhoDoFilho);
+
+console.log(document.getElementsByClassName('primeiroFilhoDoFilhoDoFilho')[0].parentElement.parentElement.nextElementSibling);
