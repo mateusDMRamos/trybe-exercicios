60d4c9ecc1b80a48f4840d0a5849db4cf99b265a
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-07-28 09:47:41 -0300

    criadas linhas de navegação entre os elementos a partir do exercício solicitado

--- fundamentos/Bloco-05-JS-DOM-Eventos-Web-Storage/5.2-Trabalhando-com-Elementos/script.js
+++ fundamentos/Bloco-05-JS-DOM-Eventos-Web-Storage/5.2-Trabalhando-com-Elementos/script.js
@@ -1,1 +1,13 @@
 document.getElementById('elementoOndeVoceEsta').parentElement.style.color = "blue";
+
+document.getElementById('primeiroFilhoDoFilho').innerText = "Jorginho";
+
+console.log(document.getElementById('pai').firstElementChild);
+
+console.log(document.getElementById('elementoOndeVoceEsta').previousElementSibling);
+
+console.log(document.getElementById('elementoOndeVoceEsta').nextSibling);
+
+console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling);
+
+console.log(document.getElementById('pai').children[2]);
