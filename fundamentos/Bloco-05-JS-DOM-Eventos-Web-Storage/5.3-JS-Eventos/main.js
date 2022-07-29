6eb03826c115547f8351eacaec92f7feba5b70f5
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-07-29 15:07:29 -0300

    criada lista de  dias pelo JS, cada dia recebeu a classe 'day' e caso fosse sexta, a classe 'friday'. os feriados receberam a classe 'holiday'

--- fundamentos/Bloco-05-JS-DOM-Eventos-Web-Storage/5.3-JS-Eventos/main.js
+++ fundamentos/Bloco-05-JS-DOM-Eventos-Web-Storage/5.3-JS-Eventos/main.js
@@ -9,8 +9,26 @@ function createDaysOfTheWeek() {
   
       weekDaysList.appendChild(dayListItem);
     };
-  };
+};
   
-  createDaysOfTheWeek();
+createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+//🚀 Exercício 1:
+let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+let listaDias = document.getElementById('days');
+for (let index in decemberDaysList) {
+    let dia = document.createElement('li');
+    dia.innerText = decemberDaysList[index];
+    if (dia.innerText ==24 || dia.innerText ==31) {
+        dia.className = "day holiday";
+    } else if (dia.innerText ==4 || dia.innerText ==11 || dia.innerText ==18) {
+        dia.className = "day friday";        
+    } else if (dia.innerText ==25) {
+        dia.className = "day holiday friday";
+    } else {
+        dia.className = "day";
+    }
+    listaDias.appendChild(dia);
+}
