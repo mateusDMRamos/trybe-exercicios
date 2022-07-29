8f7ad0c0fde2ae47e9b5987d04e99207847eac9b
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-07-29 13:07:13 -0300

    correção na função de alteração de classe e inclusão da função para alterar os textos das boxes

--- fundamentos/Bloco-05-JS-DOM-Eventos-Web-Storage/5.3-JS-Eventos/main.js
+++ fundamentos/Bloco-05-JS-DOM-Eventos-Web-Storage/5.3-JS-Eventos/main.js
@@ -15,8 +15,8 @@ const myWebpage = document.getElementById('my-spotrybefy');
 
 let container = document.getElementsByClassName('container')[0].children;
 
-for (let listagem in container) {
-    container[listagem].addEventListener('click',mudaClass);
+for (let index = 0; index < container.length; index += 1) {
+    container[index].addEventListener('click',mudaClass);
 }
 function mudaClass (opcao) {
     let target = opcao.target;
@@ -35,10 +35,17 @@ function mudaClass (opcao) {
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+input.addEventListener('keyup',alteraTexto);
+function alteraTexto () {
+    document.getElementsByClassName('tech')[0].innerText = input.value;
+}
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+
+
+
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
