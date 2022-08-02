810e32e4c84158674ca8957197c4536f6fc22333
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2022-08-02 08:05:55 -0300

    arquivo modificado com funções para mudar algumas características e valores já estão sendo armazenados no localStorage

--- fundamentos/Bloco-05-JS-DOM-Eventos-Web-Storage/5.4-Web-Storage/script.js
+++ fundamentos/Bloco-05-JS-DOM-Eventos-Web-Storage/5.4-Web-Storage/script.js
@@ -1,25 +1,74 @@
-const button = document.getElementById('add-button');
-const input = document.getElementById('phrases-input');
-const list = document.getElementById('phrases-list');
+const corDeFundo = document.getElementById('backgroundChange');
+const corDaLetra = document.getElementById('fontColorChange');
+const aumentoFonte = document.getElementById('sizeUp');
+const reducaoFonte = document.getElementById('sizeDown');
+const aumentoEspacamento = document.getElementById('spaceUp');
+const reducaoEspacamento = document.getElementById('spaceDown');
+const fontFamily = document.getElementById('fontFamilyChange');
+const caracteristicas = ['backgroundColor', 'color', 'fontSize', 'lineHeight', 'fontFamily'];
 
-function insertPhraseInDOM() {
-  const phrasesList = JSON.parse(sessionStorage.getItem('phrases'));
-  const listLength = phrasesList.length - 1;
-  const phraseText = phrasesList[listLength];
-  const phrase = document.createElement('li');
-  phrase.innerText = phraseText;
-  list.appendChild(phrase);
+corDeFundo.addEventListener('change', () => {
+    if (corDeFundo.value !== 'random'){
+        document.body.style.backgroundColor = corDeFundo.value;
+        localStorage.setItem('backgroundColor', corDeFundo.value);
+    } else {
+        let rgb = 'rgb(' + Math.random ()*255 + ', ' + Math.random ()*255 + ', ' + Math.random ()*255 + ')';
+        document.body.style.backgroundColor = rgb;
+        localStorage.setItem('backgroundColor', rgb);
+    }
+});
+
+corDaLetra.addEventListener('change', () => {
+    if (corDaLetra.value !== 'random'){
+        document.body.style.color = corDaLetra.value;
+        localStorage.setItem('color', corDaLetra.value);
+    } else {
+        let rgb = 'rgb(' + Math.random ()*255 + ', ' + Math.random ()*255 + ', ' + Math.random ()*255 + ')';
+        document.body.style.color = rgb;
+        localStorage.setItem('color', rgb);
+    }
+});
+
+aumentoFonte.addEventListener('click', () => {
+    let tamanhoFonte = parseInt(getComputedStyle(document.body).fontSize) + 1 + 'px';
+    document.body.style.fontSize = tamanhoFonte;
+    localStorage.setItem('fontSize', tamanhoFonte);
+});
+
+reducaoFonte.addEventListener('click', () => {
+    let tamanhoFonte = parseInt(getComputedStyle(document.body).fontSize) - 1 + 'px';
+    document.body.style.fontSize = tamanhoFonte;
+    localStorage.setItem('fontSize', tamanhoFonte);
+});
+
+aumentoEspacamento.addEventListener('click', () => {
+    let tamanhoEspaco = parseInt(getComputedStyle(document.body).lineHeight) + 1 + 'px';
+    document.body.style.lineHeight = tamanhoEspaco;
+    localStorage.setItem('lineHeight', tamanhoEspaco);
+});
+
+reducaoEspacamento.addEventListener('click', () => {
+    let tamanhoEspaco = parseInt(getComputedStyle(document.body).lineHeight) - 1 + 'px';
+    document.body.style.lineHeight = tamanhoEspaco;
+    localStorage.setItem('lineHeight', tamanhoEspaco);
+});
+
+fontFamily.addEventListener('change', () => {
+    document.body.style.fontFamily = fontFamily.value; 
+    localStorage.setItem('fontFamily', fontFamily.value);
+});
+
+window.onload = () => {
+    for (let index = 0; index <= caracteristicas.length; index += 1) {
+        if (typeof(Storage) !== undefined) {
+            if (localStorage.getItem(caracteristicas[index]) == undefined) {
+                let caracteristica = caracteristicas[index]
+                let valorInicial = JSON.stringify(document.body.style.caracteristica);
+                localStorage.setItem(caracteristica, valorInicial);
+            } else {
+                let caracteristica = caracteristicas[index]
+                document.body.style.caracteristica = localStorage.getItem(caracteristica);
+            }
+        }
+    }
 }
-
-function addPhraseToSessionStorage() {
-  if (sessionStorage.getItem('phrases') === null) {
-    sessionStorage.setItem('phrases', JSON.stringify([]));
-  }
-  const oldList = JSON.parse(sessionStorage.getItem('phrases'));
-  const phraseText = input.value;
-  oldList.push(phraseText);
-  sessionStorage.setItem('phrases', JSON.stringify(oldList));
-  insertPhraseInDOM();
-}
-
-button.addEventListener('click', addPhraseToSessionStorage);
